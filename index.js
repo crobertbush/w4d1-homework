6395bcbaae0fa1a385fff57a5672e6c83adb5a60
Robby Bush <crobertbush@gmail.com>
2022-04-18 17:00:20 -0400

    homework

---
+++ index.js
@@ -0,0 +1,34 @@
+//Lesson 1
+var lesson1 = [];
+
+for (let i = 1; i < 11; i++){
+    lesson1.push(i);
+}
+console.log(lesson1);
+
+
+//Lesson 2
+var lesson2 = [];
+let i = 1;
+
+while (i < 11){
+    lesson2.push(i);
+    i++;
+}
+console.log(lesson2);
+
+
+//Lesson 3
+var lesson3 = [];
+for (let i = 11; i <= 21; i += 2){
+    lesson3.push(i);
+}
+console.log(lesson3);
+
+
+//Lesson 4
+var lesson4 = [];
+for (let i = 15; i > 0; i -= 2){
+    lesson4.push(i);
+}
+console.log(lesson4);
